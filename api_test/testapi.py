11e8be1fb811d72145ac3832078e484c122b259c
rishabh <rishabh@roofpik.com>
2017-02-03 18:15:07 +0530

    some change

--- api_test/testapi.py
+++ api_test/testapi.py
@@ -219,9 +219,9 @@ class testclass(Resource):
                     while i<_page_size:
                         d = {}
                         d.update({'id':res['hits']['hits'][i]['_source']['projectId']})
-                        #d.update('name': res['hits']['hits'][i]['_source']['details.name'])
-                        d.update({'adddress': res['hits']['hits'][i]['_source']['address']})
-                        # d.update({'cover': res['hits']['hits'][i]['_source']['cover_pic']})
+                        d.update('name': res['hits']['hits'][i]['_source']['details.name'])
+                        d.update({'address': res['hits']['hits'][i]['_source']['address']})
+                        d.update({'cover': res['hits']['hits'][i]['_source']['cover_pic']})
                         x.update(d)
                         i += 1
                     return x    
