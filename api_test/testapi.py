70bf0bb45d218cbfe6c47be39d66ff0a9aba3aec
rishabh <rishabh@roofpik.com>
2017-02-03 18:19:07 +0530

    some change

--- api_test/testapi.py
+++ api_test/testapi.py
@@ -222,6 +222,7 @@ class testclass(Resource):
                         d.update('name': res['hits']['hits'][i]['_source']['details.name'])
                         d.update({'address': res['hits']['hits'][i]['_source']['address']})
                         d.update({'cover': res['hits']['hits'][i]['_source']['cover_pic']})
+                        return d
                         x.update({i:d})
                         i += 1
                     return x    
