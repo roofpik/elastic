0bee977bd49f24ea8053d446f5518b962891d38b
root <root@api.roofpik.com>
2017-02-06 19:47:40 +0530

    fixing

--- api_test_residential/api_residential.py
+++ api_test_residential/api_residential.py
@@ -254,8 +254,9 @@ class residentialclass(Resource):
 						j += 1
 						z += 1
 	
-			return query_builder
+#			return query_builder
 			res = es.search(index='res_index', doc_type='data', body=query_builder, from_=_page_start, size=_page_size)
+			return res
 			index_num = 0
 			final_res = {}
 			temp_res = {}
