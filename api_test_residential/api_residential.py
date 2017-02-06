05502690f6ca9de9c1ced413f15f5874da83ccdd
rishabh <rishabh@roofpik.com>
2017-02-06 16:51:49 +0530

    debug

--- api_test_residential/api_residential.py
+++ api_test_residential/api_residential.py
@@ -269,7 +269,7 @@ class residentialclass(Resource):
 						temp_res.update({index_num : temp_temp_res})
 						index_num += 1
 					final_res.update({'details': temp_res})
-					return res    
+					return final_res    
 
 				except Exception:
 					return Exception
