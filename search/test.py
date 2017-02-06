421d5c95851e54d6160b29871579e1adb805acde
rishabh <rishabh@roofpik.com>
2017-02-06 13:56:00 +0530

    test try

--- search/test.py
+++ search/test.py
@@ -6,5 +6,5 @@ from elasticsearch import Elasticsearch
 class test(Resource):
 	def get(self):
 		es = Elasticsearch([{'host': 'localhost', 'port': 9200}])
-		t = es.get(index='live_index_1', doc_type='data')
+		t = es.search(index='live_index_1', doc_type='data')
 		return t
