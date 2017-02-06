50cfbdd3b42187ead35547fd8244e1f3b70f3664
rishabh <rishabh@roofpik.com>
2017-02-06 14:04:39 +0530

    updated query in test

--- search/test.py
+++ search/test.py
@@ -6,5 +6,5 @@ from elasticsearch import Elasticsearch
 class test(Resource):
 	def get(self):
 		es = Elasticsearch([{'host': 'localhost', 'port': 9200}])
-		t = es.search(index='live_index_1', doc_type='data')
+		t = es.search(index='live_index_1', doc_type='data', body={"query": {"match":{"projectId": "-Ka2SmvifAG0CKkfsjyw"}}})
 		return t
