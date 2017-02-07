6f576df9581c63827a6f1fd8d2e3a915ddac4df8
rishabh <rishabh@roofpik.com>
2017-02-07 13:57:32 +0530

    api res review

--- api_reviews_residential/api_res_reviews.py
+++ api_reviews_residential/api_res_reviews.py
@@ -36,8 +36,8 @@ def getParamsRating(query_builder, temp_num_reviews, es):
 	ratingParams = ['layoutOfApartment', 'electricityAndWaterSupply', 'convenienceOfParking', 'openAndGreenAreas', 'convenienceOfHouseMaids', 'infrastructure', 'amenities', 'security']
 	k = 0
 	while k<8:
+		return query_builder
 		temp_s = es.search(index='res_reviews', doc_type='reviews', body=checkExistance(query_builder, 'ratings.'+ratingParams[k]), size=temp_num_reviews)
-		return '00000000'
 		j = 0
 		while j<temp_s['hits']['total']: 
 			ratingParamsRating[k] = ratingParamsRating[k] + int(temp_s['hits']['hits'][j]['_source']['ratings'][ratingParams[k]])
