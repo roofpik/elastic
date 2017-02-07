95d6ca31a56fc722074da800696cd73ea2104c35
rishabh <rishabh@roofpik.com>
2017-02-07 16:55:07 +0530

    api res fix

--- api_test_residential/api_residential.py
+++ api_test_residential/api_residential.py
@@ -267,7 +267,7 @@ class residentialclass(Resource):
 						query_builder = build_query_should("amenities."+temp[z], "Yes", query_builder, j)
 						j += 1
 						z += 1
-			#return query_builder
+			return query_builder
 			res = es.search(index='res_index', doc_type='data', body=query_builder, from_=_page_start, size=_page_size)
 			index_num = 0
 			final_res = {}
@@ -291,7 +291,6 @@ class residentialclass(Resource):
 				temp_temp_res.update({'bhks': fbhk})
 				temp_res.update({index_num : temp_temp_res})
 				index_num += 1
-				return temp_res
 
 			final_res.update({'details': temp_res})
 			return final_res
