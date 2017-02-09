613374c1621a8cdae6793d81870f1c8e575a0135
rishabh-roofpik <rishabh@roofpik.com>
2017-02-09 18:39:50 +0530

    api cghs creation

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from flask.ext.cors import CORS
 
 from search import *
 from residential import *
+from cghs import *
 from residential_review_1 import *
 from residential_review_2 import *
 from residential_review_3 import *
@@ -14,6 +15,7 @@ CORS(app)
 
 api.add_resource(test,'/test')
 api.add_resource(residentialclass,'/residentialProjects')
+api.add_resource(cghsclass,'/cghsProjects')
 api.add_resource(residentialreview1class,'/reviewSummary')
 api.add_resource(residentialreview2class,'/reviewDetails')
 api.add_resource(residentialreview3class,'/projectReviews')
---
+++ cghs/__init__.py
@@ -0,0 +1,1 @@
+from cghs import *
