9250f889a12950cb2f18b0a0c3f854cc08514252
rishabh-roofpik <rishabh@roofpik.com>
2017-02-09 14:28:29 +0530

    api res review 3

--- residential_review_3/residential_review_3.py
+++ residential_review_3/residential_review_3.py
@@ -60,7 +60,7 @@ class residentialreview3class(Resource):
 				i += 1
 
 			url = 'https://search-roof-pnslfpvdk2valk5lfzveecww54.ap-south-1.es.amazonaws.com/res_reviews/reviews/_search'
-			return query_builder
+
 			r = requests.post(url, data=query_builder)
 			r = json.loads(r.text)
 			return r
