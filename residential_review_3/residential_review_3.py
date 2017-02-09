ab06b851dc2fbff453a6cc417e5b726767b30433
rishabh-roofpik <rishabh@roofpik.com>
2017-02-09 15:14:31 +0530

    api res review 3 fix

--- residential_review_3/residential_review_3.py
+++ residential_review_3/residential_review_3.py
@@ -70,7 +70,6 @@ class residentialreview3class(Resource):
 			if(_overallRating):
 				query_builder = build_query_must_range(_overallRating, query_builder, i)
 				i += 1
-			return 'works'
 			
 			url = 'https://search-roof-pnslfpvdk2valk5lfzveecww54.ap-south-1.es.amazonaws.com/res_reviews/reviews/_search?size='+_page_size+'&from='+_page_start	
 
