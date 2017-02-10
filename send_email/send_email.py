904553ac0546fa351ee714567776b326348dfe46
rishabh-roofpik <rishabh@roofpik.com>
2017-02-10 11:45:29 +0530

    added email api

--- send_email/send_email.py
+++ send_email/send_email.py
@@ -6,4 +6,11 @@ from flask import *
 
 class sendemailclass(Resource):
 	def get(self):
-		return 'this works'
+
+		parser = reqparse.RequestParser()
+		parser.add_argument('email', type=str)
+		args = parser.parse_args()
+		_email = args['email']
+
+		_email = _email.decode('base64')
+		return email
