d918533711434f1fa526ab1578b66dbb95e848b1
rishabh-roofpik <rishabh@roofpik.com>
2017-02-13 10:45:25 +0530

    fix locality search api

--- locality_search/locality_search.py
+++ locality_search/locality_search.py
@@ -16,8 +16,8 @@ class localityclass(Resource):
 		#_id = decodeArgs(_id)
 		#_page_start = _id[1]
 		#_page_size = _id[2]
-		_page_start = 0
-		_page_size = 10
+		_page_start = '0'
+		_page_size = '10'
 
 		url = 'https://search-roof-pnslfpvdk2valk5lfzveecww54.ap-south-1.es.amazonaws.com/locality_index/data/_search?size='+_page_size+'&from='+_page_start
 
@@ -29,8 +29,8 @@ class localityclass(Resource):
 			r_temp = {}
 			d_temp = {}
 			d_temp.update({'hits' : count})
-			if(count>_page_size):
-				count = _page_size 
+			if(count>int(_page_size)):
+				count = int(_page_size) 
 			while index<count:
 				r_temp.update({index : res['hits']['hits'][index]['_source']})
 				index += 1
