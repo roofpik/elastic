9aec775c692e05b9eac8de3c27c5dc4f42904590
rishabh-roofpik <rishabh@roofpik.com>
2017-02-13 16:04:59 +0530

    added loc distance api

---
+++ distance_loc/distance_loc.py
@@ -0,0 +1,56 @@
+from restful import Resource
+import requests
+import json
+from flask_restful import reqparse
+from flask import *
+
+class locationdistanceclass(Resource):
+	def get(self):
+
+		parser = reqparse.RequestParser()		
+		
+		#parser.add_argument('args', type=str)
+		#args = parser.parse_args()
+		#_args = args['args']
+
+		#if not _args:
+		#	return 'provide arguments'
+
+		parser.add_argument('lat', type=str)
+		args = parser.parse_args()
+		_lat = args['lat']
+		if not _lat:
+			_lat = "0"
+
+		parser.add_argument('lon', type=str)
+		args = parser.parse_args()
+		_lon = args['lon']
+		if not _lon:
+			_lon = "0"
+		
+		_page_start = '0'
+		_page_size = '5'
+
+		query = { "sort": [ { "_geo_distance": { "location": { "lat": float(lat), "lon": float(lon) }, "order": "asc", "unit": "km", "distance_type": "plane" } } ] }
+		
+		query = json.dumps(query)
+
+		url = 'https://search-roof-pnslfpvdk2valk5lfzveecww54.ap-south-1.es.amazonaws.com/res_reviews/reviews/_search?size='+_page_size+'&from='+_page_start
+
+		res = requests.post(url, data=query)
+		res = json.loads(res.text)
+		res_count = res['hits']['total']
+
+		if(res_count <= 10):
+			page_counter = res_count
+		else:
+			page_counter = int(_page_size)
+
+		index=0
+		display_result = {}
+		display_result.update({'hits' : res['hits']['total']})
+		while index<page_counter:
+			temp_result = {}
+			temp_result.update({index : res['hits']['hits'][index]['_source']})
+		display_result.update({'details' : temp_result})
+		return display_result
