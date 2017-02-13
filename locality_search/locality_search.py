4acf9225a29e15e4eb4538096c4631c5614d71fc
rishabh-roofpik <rishabh@roofpik.com>
2017-02-13 10:44:27 +0530

    fix locality search api

--- locality_search/locality_search.py
+++ locality_search/locality_search.py
@@ -8,11 +8,18 @@ from decoder import decodeArgs
 class localityclass(Resource):
 	def get(self):
 
-		url = 'https://search-roof-pnslfpvdk2valk5lfzveecww54.ap-south-1.es.amazonaws.com/locality_index/data/_search'
 		parser = reqparse.RequestParser()
 		parser.add_argument('id', type=str)
 		args = parser.parse_args()
 		_id = args['id']
+
+		#_id = decodeArgs(_id)
+		#_page_start = _id[1]
+		#_page_size = _id[2]
+		_page_start = 0
+		_page_size = 10
+
+		url = 'https://search-roof-pnslfpvdk2valk5lfzveecww54.ap-south-1.es.amazonaws.com/locality_index/data/_search?size='+_page_size+'&from='+_page_start
 
 		if not _id:
 			res = requests.get(url)
@@ -22,17 +29,16 @@ class localityclass(Resource):
 			r_temp = {}
 			d_temp = {}
 			d_temp.update({'hits' : count})
+			if(count>_page_size):
+				count = _page_size 
 			while index<count:
 				r_temp.update({index : res['hits']['hits'][index]['_source']})
-				return res
 				index += 1
 			d_temp.update({'details' : r_temp})
 			return d_temp
 
-		#_id = decodeArgs(_id)
 
 		query = {"query": {"match": {"name": _id}}}
-		#return 'do'
 		query = json.dumps(query)
 		res = requests.post(url, data = query)
 		res = json.loads(res.text)
