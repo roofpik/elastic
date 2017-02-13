d8ae2bf2fd6a3b4246f65aa994e7af1162f86ad7
rishabh-roofpik <rishabh@roofpik.com>
2017-02-13 10:20:37 +0530

    added locality search api

---
+++ locality_search/locality_search.py
@@ -0,0 +1,24 @@
+from restful import Resource
+import requests
+import json
+from flask_restful import reqparse
+from flask import *
+from decoder import decodeArgs
+
+class localityclass(Resource):
+	def get(self):
+		
+		parser = reqparse.RequestParser()
+		parser.add_argument('id', type=str)
+		args = parser.parse_args()
+		_id = args['id']
+
+		#_id = decodeArgs(_id)
+	
+		url = 'https://search-roof-pnslfpvdk2valk5lfzveecww54.ap-south-1.es.amazonaws.com/locality_index/data/_search'
+
+		query = {"query": {"match": {"name": _id}}}
+
+		res = requests(url, d = query)
+		res = json.loads(res.text)
+		return res
