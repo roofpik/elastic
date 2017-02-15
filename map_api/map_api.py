b4503877ebc1903670d8bfb442676c2bfa8548b5
rishabh-roofpik <rishabh@roofpik.com>
2017-02-15 14:13:56 +0530

    map api change

--- map_api/map_api.py
+++ map_api/map_api.py
@@ -15,35 +15,51 @@ def getRating(id, url):
 	except:
 		return 0
 
-def getProjects(location_id, url, url4, temp3, _type):
-	for l in location_id:
-		query = {"query":{"match":{"location."+str(l) : True}}}
-		query = json.dumps(query)
-		res1 = requests.post(url, data = query)
-		res1 = json.loads(res1.text)
-		size = res1['hits']['total']
-		res1 = requests.post(url+'?size='+str(size), data = query)
-		res1 = json.loads(res1.text)
-		i=0
-		temp2 = {}
-		while i<res1['hits']['total']:
-			temp1 = {}
-			temp1.update({'pid':res1['hits']['hits'][i]['_source']['projectId']})
-			id = res1['hits']['hits'][i]['_source']['projectId']
-			rating = getRating(id, url4)
-			temp1.update({'rating':rating})
-			temp1.update({'cover':res1['hits']['hits'][i]['_source']['cover_pic']})
-			temp1.update({'rent':res1['hits']['hits'][i]['_source']['rent']})
-			temp1.update({'location':res1['hits']['hits'][i]['_source']['coordinates']})
-			temp1.update({'type':_type})
-			temp2.update({i:temp1})
-			i += 1
-		temp3.update({str(l):temp2})
-		return temp3
+def getLocations(distance_query, url1):
+	res = requests.post(url1, data=distance_query)
+	res = json.loads(res.text)
+	res = requests.post(url1+'?size='+str(res['hits']['total']), data=distance_query)
+	res = json.loads(res.text)
+	temp2 = {}
+	while i<res1['hits']['total']:
+		temp1 = {}
+		temp1.update({'id':res1['hits']['hits'][i]['_source']['id']})
+		temp1.update({'rating':'not available yet'})
+		temp1.update({'cover':'not available yet'})
+		temp1.update({'rent':'not available yet'})
+		temp1.update({'location':res1['hits']['hits'][i]['_source']['location']})
+		temp1.update({'type':res1['hits']['hits'][i]['_source']['type']})
+		temp1.update({'name':res1['hits']['hits'][i]['_source']['name']})
+		temp2.update({id:temp1})
+		i += 1
+	return temp2
+
+def getProjects(distance_query, url, project_type, url4):
+	res1 = requests.post(url, data = distance_query)
+	res1 = json.loads(res1.text)
+	size = res1['hits']['total']
+	res1 = requests.post(url+'?size='+str(size), data = distance_query)
+	res1 = json.loads(res1.text)
+	i=0
+	temp2 = {}
+	while i<res1['hits']['total']:
+		temp1 = {}
+		temp1.update({'id':res1['hits']['hits'][i]['_source']['projectId']})
+		id = res1['hits']['hits'][i]['_source']['projectId']
+		rating = getRating(id, url4)
+		temp1.update({'rating':rating})
+		temp1.update({'cover':res1['hits']['hits'][i]['_source']['cover_pic']})
+		temp1.update({'rent':res1['hits']['hits'][i]['_source']['rent']})
+		temp1.update({'location':res1['hits']['hits'][i]['_source']['coordinates']})
+		temp1.update({'name':res1['hits']['hits'][i]['_source']['details.name']})
+		temp1.update({'type':project_type})
+		temp2.update({id:temp1})
+		i += 1
+	return temp2
 
 class mapapiclass(Resource):
 	def get(self):
-
+		
 		url1 = 'https://search-roof-pnslfpvdk2valk5lfzveecww54.ap-south-1.es.amazonaws.com/locality_geo/data/_search'
 		url2 = 'https://search-roof-pnslfpvdk2valk5lfzveecww54.ap-south-1.es.amazonaws.com/cghs_index/data/_search'
 		url3 = 'https://search-roof-pnslfpvdk2valk5lfzveecww54.ap-south-1.es.amazonaws.com/residential_index/data/_search'
@@ -69,21 +85,17 @@ class mapapiclass(Resource):
 		else:
 			_distance = '5'
 
-		distance_query = { "query": { "bool" : { "must" : { "match_all" : {} }, "filter" : { "geo_distance" : { "distance" : _distance+"km", "location" : { "lat" : _lat, "lon" : _lon } } } } } }
+		distance_query_location = { "query": { "bool" : { "must" : { "match_all" : {} }, "filter" : { "geo_distance" : { "distance" : _distance+"km", "location" : { "lat" : _lat, "lon" : _lon } } } } } }
 
-		distance_query = json.dumps(distance_query)
+		distance_query_location = json.dumps(distance_query_location)
 
-		res = requests.post(url1, data=distance_query)
-		res = json.loads(res.text)
-		res = requests.post(url1+'?size='+str(res['hits']['total']), data=distance_query)
-		res = json.loads(res.text)
-		location_id = []
-		i = 0
-		while i<res['hits']['total']:
-			location_id.append(res['hits']['hits'][i]['_source']['id'])
-			i += 1
+		distance_query_projects = { "query": { "bool" : { "must" : { "match_all" : {} }, "filter" : { "geo_distance" : { "distance" : _distance+"km", "coordinates" : { "lat" : _lat, "lon" : _lon } } } } } }
 
-		temp3 = {}
-		temp3 = getProjects(location_id, url2, url4, temp3, "cghs")
-		temp3 = getProjects(location_id, url3, url4, temp3, "residential")
-		return temp3
+		distance_query_projects = json.dumps(distance_query_projects)
+
+		result = {}
+		result.update(getLocations(distance_query_location, url1))
+		result.update(getProjects(distance_query_projects, url2, "cghs", url4))
+		result.update(getProjects(distance_query_projects, url3, "residential", url4))
+
+		return result
