80f41f18c8044ee7c1df39fd1a8681592b8d8d77
rishabh-roofpik <rishabh@roofpik.com>
2017-02-15 14:23:10 +0530

    map api fix

--- map_api/map_api.py
+++ map_api/map_api.py
@@ -52,7 +52,7 @@ def getProjects(distance_query, url, project_type, url4):
 		temp1.update({'cover':res1['hits']['hits'][i]['_source']['cover_pic']})
 		temp1.update({'rent':res1['hits']['hits'][i]['_source']['rent']})
 		temp1.update({'location':res1['hits']['hits'][i]['_source']['coordinates']})
-		temp1.update({'name':res1['hits']['hits'][i]['_source']['details.name']})
+		temp1.update({'name':res1['hits']['hits'][i]['_source']['details']['name']})
 		temp1.update({'type':project_type})
 		temp2.update({id:temp1})
 		i += 1
