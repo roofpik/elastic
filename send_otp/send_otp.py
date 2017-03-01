63d3d7455dafb30ec9d78c984d0e2628520b156c
rishabh-roofpik <rishabh@roofpik.com>
2017-03-01 17:55:37 +0530

    fix otp api

--- send_otp/send_otp.py
+++ send_otp/send_otp.py
@@ -17,9 +17,8 @@ def sendMail(email, _otp):
 	return 'mail sent'
 
 class sendotpclass(Resource):
-    def get(self):
-        try:
-        	return 'ok'
+	def get(self):
+		try:
 			parser = reqparse.RequestParser()
 			#requesting argument
 			parser.add_argument('args', type=str)
@@ -29,19 +28,19 @@ class sendotpclass(Resource):
 			_args = decodeArgs(_args)
 			_mobile = _args['mobile']
 			_otp = _args['otp']
-			#_email = _args['email']
+			_email = _args['email']
 			#url to hit external api
 			url = 'http://smsapi.24x7sms.com/api_2.0/SendSMS.aspx?APIKEY=rNfGwBJ7xcV&MobileNo='+_mobile+'&SenderID=ROOFPK&Message=Greetings! '+_otp+' is your verification code for Roofpik.&ServiceName=TEMPLATE_BASED'
 			response = requests.post(url)
 			status = response.status_code
 			content = response.content
 			#send mail
-			#mailStatus = sendMail(_email, _otp)
+			mailStatus = sendMail(_email, _otp)
 			#send response
 			return {
                     'status' : status,
                     'msg': 'OTP successfully sent to '+_mobile
-             #       'mail': mailStatus
+                    'mail': mailStatus
                 }
         except Exception as e:
 			return str(e)
