ba48b4928bf37824204e442b8b8b08d875631d2d
rishabh-roofpik <rishabh@roofpik.com>
2017-03-01 12:37:58 +0530

    fix sendotp

--- send_otp/send_otp.py
+++ send_otp/send_otp.py
@@ -4,16 +4,15 @@ import requests
 from decoder import decodeArgs
 
 class sendotpclass(Resource):
-    def get(self):
+    def post(self):
         try:
 			parser = reqparse.RequestParser()
 			parser.add_argument('mobile', required=True, type=int, help='user phone number')
 			parser.add_argument('otp', required=True, type=int,  help='one time password')
 			args = parser.parse_args()
-			args = decodeArgs(args)
 			_mobile = args['mobile']
 			_otp = args['otp']
-			url = 'http://smsapi.24x7sms.com/api_2.0/SendSMS.aspx?APIKEY=rNfGwBJ7xcV&MobileNo='+_mobile+'&SenderID=ROOFPK&Message=Greetings, '+_otp+' is your verification code for Roofpik.&ServiceName=TEMPLATE_BASED'
+			url = 'http://smsapi.24x7sms.com/api_2.0/SendSMS.aspx?APIKEY=rNfGwBJ7xcV&MobileNo='+str(_mobile)+'&SenderID=ROOFPK&Message=Greetings, '+str(_otp)+' is your verification code for Roofpik.&ServiceName=TEMPLATE_BASED'
 			response = requests.post(url)
 			status = response.status_code
 			content = response.content
