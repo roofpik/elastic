768e6ba512db3cee2f48b4841c6e1fe8062b4ba4
rishabh-roofpik <rishabh@roofpik.com>
2017-03-01 12:17:09 +0530

    fix indentation in sendotp

--- send_otp/send_otp.py
+++ send_otp/send_otp.py
@@ -11,17 +11,16 @@ class sendotpclass(Resource):
 			parser.add_argument('otp', required=True, type=int,  help='one time password')
 			args = parser.parse_args()
 			args = decodeArgs(args)
-            _mobile = args['mobile']
-            _otp = args['otp']
-
-            url = 'http://smsapi.24x7sms.com/api_2.0/SendSMS.aspx?APIKEY=rNfGwBJ7xcV&MobileNo='+str(_mobile)+'&SenderID=ROOFPK&Message=Greetings, '+str(otp)+' is your verification code for Roofpik.&ServiceName=TEMPLATE_BASED'
-            response = requests.post(url)
-            status = response.status_code
-            content = response.content
-            return {
+			_mobile = args['mobile']
+			_otp = args['otp']
+			url = 'http://smsapi.24x7sms.com/api_2.0/SendSMS.aspx?APIKEY=rNfGwBJ7xcV&MobileNo='+str(_mobile)+'&SenderID=ROOFPK&Message=Greetings, '+str(otp)+' is your verification code for Roofpik.&ServiceName=TEMPLATE_BASED'
+			response = requests.post(url)
+			status = response.status_code
+			content = response.content
+			return {
                     'status' : status,
                     'msg': 'OTP successfully sent to '+str(_mobile)
                 }
         except Exception as e:
-            return str(e)
+			return str(e)
 
