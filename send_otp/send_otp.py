df7b80357ecf419eb8eeec4f3fb5b38745e28708
rishabh-roofpik <rishabh@roofpik.com>
2017-03-01 12:41:30 +0530

    fix sendotp

--- send_otp/send_otp.py
+++ send_otp/send_otp.py
@@ -7,18 +7,18 @@ class sendotpclass(Resource):
     def post(self):
         try:
 			parser = reqparse.RequestParser()
-			parser.add_argument('mobile', required=True, type=int, help='user phone number')
-			parser.add_argument('otp', required=True, type=int,  help='one time password')
+			parser.add_argument('args', required=True, type=str)
 			args = parser.parse_args()
+			args=decodeArgs(args)
 			_mobile = args['mobile']
 			_otp = args['otp']
-			url = 'http://smsapi.24x7sms.com/api_2.0/SendSMS.aspx?APIKEY=rNfGwBJ7xcV&MobileNo='+str(_mobile)+'&SenderID=ROOFPK&Message=Greetings, '+str(_otp)+' is your verification code for Roofpik.&ServiceName=TEMPLATE_BASED'
+			url = 'http://smsapi.24x7sms.com/api_2.0/SendSMS.aspx?APIKEY=rNfGwBJ7xcV&MobileNo='+_mobile+'&SenderID=ROOFPK&Message=Greetings, '+_otp+' is your verification code for Roofpik.&ServiceName=TEMPLATE_BASED'
 			response = requests.post(url)
 			status = response.status_code
 			content = response.content
 			return {
                     'status' : status,
-                    'msg': 'OTP successfully sent to '+str(_mobile)
+                    'msg': 'OTP successfully sent to '+_mobile
                 }
         except Exception as e:
 			return str(e)
