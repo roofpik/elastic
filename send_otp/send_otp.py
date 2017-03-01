1be2f889533f48b1cbea5e8fbfbf66fd2d1c8d3a
rishabh-roofpik <rishabh@roofpik.com>
2017-03-01 13:11:28 +0530

    fix sendotp

--- send_otp/send_otp.py
+++ send_otp/send_otp.py
@@ -7,18 +7,20 @@ class sendotpclass(Resource):
     def get(self):
         try:
 			parser = reqparse.RequestParser()
+			#requesting argument
 			parser.add_argument('args', type=str)
 			args = parser.parse_args()
 			_args = args['args']
-		
+			#decoding arguments
 			_args = decodeArgs(_args)
-
 			_mobile = _args['mobile']
 			_otp = _args['otp']
+			#url to hit external api
 			url = 'http://smsapi.24x7sms.com/api_2.0/SendSMS.aspx?APIKEY=rNfGwBJ7xcV&MobileNo='+_mobile+'&SenderID=ROOFPK&Message=Greetings! '+_otp+' is your verification code for Roofpik.&ServiceName=TEMPLATE_BASED'
 			response = requests.post(url)
 			status = response.status_code
 			content = response.content
+			#ssend response
 			return {
                     'status' : status,
                     'msg': 'OTP successfully sent to '+_mobile
