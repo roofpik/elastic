cfa71deae248b5bb05187656f26e8ad6fbf92c86
rishabh-roofpik <rishabh@roofpik.com>
2017-03-01 12:53:44 +0530

    fix sendotp

--- send_otp/send_otp.py
+++ send_otp/send_otp.py
@@ -17,6 +17,7 @@ class sendotpclass(Resource):
 			_otp = _args['otp']
 			url = 'http://smsapi.24x7sms.com/api_2.0/SendSMS.aspx?APIKEY=rNfGwBJ7xcV&MobileNo='+_mobile+'&SenderID=ROOFPK&Message=Greetings, '+_otp+' is your verification code for Roofpik.&ServiceName=TEMPLATE_BASED'
 			response = requests.post(url)
+			return response
 			status = response.status_code
 			content = response.content
 			return {
