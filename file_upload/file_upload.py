5c01978d81e5e01750079203ffe1d6c5d781f271
rishabh-roofpik <rishabh@roofpik.com>
2017-03-14 12:05:57 +0530

    fix api file upload removed reqparse

--- file_upload/file_upload.py
+++ file_upload/file_upload.py
@@ -8,19 +8,20 @@ import uuid
 
 class uploadFile(Resource):
 	def post(self):
-		parser = reqparse.RequestParser()
-		parser.add_argument('name', type=str, help='name of file')
-		parser.add_argument('path', type=str, help='path to save file')
-		args = parser.parse_args()
+		# parser = reqparse.RequestParser()
+		# parser.add_argument('name', type=str, help='name of file')
+		# parser.add_argument('path', type=str, help='path to save file')
+		# args = parser.parse_args()
 		uFile = request.files['file']
-		return uFile
-        try:
-        	_path = args['path']
-        	_name = args['name']
-        except:
-        	_path = ''
-        	_name = uFile.filename
 		
+        # try:
+        # 	_path = args['path']
+        # 	_name = args['name']
+        # except:
+        # 	_path = ''
+        # 	_name = uFile.filename
+		_path = ''
+		_name = uFile.filename
 		os.chdir('/var/www/api/uploaded_files/'+_path)
 		r = uFile.save(secure_filename(_name))
 		
