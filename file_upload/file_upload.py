20d8813ceb60ecd6da7d0173c210bd6528c7fdd4
rishabh-roofpik <rishabh@roofpik.com>
2017-03-14 12:17:08 +0530

    modify file upload api

--- file_upload/file_upload.py
+++ file_upload/file_upload.py
@@ -8,20 +8,21 @@ import uuid
 
 class uploadFile(Resource):
 	def post(self):
-		# parser = reqparse.RequestParser()
-		# parser.add_argument('name', type=str, help='name of file')
-		# parser.add_argument('path', type=str, help='path to save file')
-		# args = parser.parse_args()
+		parser = reqparse.RequestParser()
+		parser.add_argument('name', type=str, help='name of file')
+		parser.add_argument('path', type=str, help='path to save file')
+		args = parser.parse_args()
 		uFile = request.files['file']
 		
-        # try:
-        # 	_path = args['path']
-        # 	_name = args['name']
-        # except:
-        # 	_path = ''
-        # 	_name = uFile.filename
-		_path = ''
-		_name = uFile.filename
+        try:
+        	_path = args['path']
+        	_name = args['name']
+        except:
+        	_path = ''
+        	_name = uFile.filename
+		# _path = ''
+		# _name = uFile.filename
 		os.chdir('/var/www/api/uploaded_files/'+_path)
 		r = uFile.save(secure_filename(_name))
+		return r
 		
